8b472d00768696029410c2871179941390e213b1
Shahid Ansari <113099652+shahidansari310@users.noreply.github.com>
2025-02-22 22:13:53 +0530

    linethrough onclicking checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
 import Footer from './components/Footer'
-
+import { v4 as uuidv4 } from 'uuid';
+ 
 function App() {
 
   const [todo, settodo] = useState("")
@@ -17,13 +18,23 @@ function App() {
   }
 
   const handleadd = () => {
-    settodos([...todos, { todo, isCompleted: false }])
+    settodos([...todos, {id:uuidv4(), todo, isCompleted: false }])
     settodo("")
     console.log(todos)
   }
 
   const handlechange = (e) => {
     settodo(e.target.value)
+  }
+
+  const handleCheckbox=(e)=>{
+    let id=e.target.name;
+    let i=todos.findIndex(item=>{
+      return item.id==id;
+    })
+    let newtodos=[...todos];
+    newtodos[i].isCompleted=!newtodos[i].isCompleted;
+    settodos(newtodos)
   }
 
   return (
@@ -39,8 +50,9 @@ function App() {
         <div className="todos">
           {todos.map((items, index) => {
             return (
-              <div key={index} className="todo flex p-1">
-                <div className={items.isCompleted? "":"line-through"}>{items.todo}</div>
+              <div key={items.id} className="todo flex justify-between p-1">
+                <input name={items.id} onChange={handleCheckbox} type="checkbox" value={items.isCompleted} />
+                <div className={items.isCompleted? "line-through":""}>{items.todo}</div>
                 <div className="button">
                   <button onClick={handledelete} className='rounded-xl bg-green-200 p-1 mx-2 font-bold ring-1 hover:text-sm'>Edit</button>
                   <button onClick={handleedit} className='rounded-xl bg-green-200 p-1 mx-2 font-bold ring-1 hover:text-sm'>Delete</button>
